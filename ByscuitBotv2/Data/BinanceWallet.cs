da472166ca68bf20393e3c026c13ec28ff10f2c5
Marcus Escobedo <marcus14530@gmail.com>
2021-04-30 16:50:24 -0700

    Added binance smart chain wallet functionality

---
+++ ByscuitBotv2/Data/BinanceWallet.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using Nethereum.HdWallet;
+using System.IO;
+using Newtonsoft.Json;
+using Discord.WebSocket;
+using System.Numerics;
+using NBitcoin;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace ByscuitBotv2.Data
+{
+    public class BinanceWallet
+    {
+        static string MAIN_NET = "https://bsc-dataseed1.binance.org:443";
+        static string TEST_NET = "https://data-seed-prebsc-1-s1.binance.org:8545";
+        static string CURRENT_NET = TEST_NET;
+        public static Web3 web3 = new Web3(CURRENT_NET);
+        static Random random = new Random();
+
+        public class WalletAccount
+        {
+            public ulong DiscordID;
+            // Store whole wallet object maybe?
+            //public Wallet Wallet;
+            public string Words; // Wont let me store words as a string array
+            public string Password;
+            public string Seed;
+            public int Index;
+            public string PrivateKey;
+            public string Address;
+
+            public WalletAccount(string words, string pass, ulong discordID)
+            {
+                Index = 0; // Hardcoded index for now
+                Password = pass; // ------------ Encode the password!!!!!! -------------
+                Words = words;
+                Wallet wallet = new Wallet(Words, Password);
+                var Account = wallet.GetAccount(Index);
+                
+                PrivateKey = Account.PrivateKey;
+                Seed = wallet.Seed;
+                Address = Account.Address;
+                DiscordID = discordID;
+            }
+
+            public Wallet GetWallet()
+            {
+                return new Wallet(Words, Password);
+            }
+
+            public Nethereum.Web3.Accounts.Account GetAccount()
+            {
+                return GetWallet().GetAccount(Index);
+            }
+
+            public async Task<TransactionReceipt> SendBNB(string to, decimal amount)
+            {
+                Web3 sweb3 = new Web3(GetAccount(), CURRENT_NET);
+                TransactionReceipt receipt = await sweb3.Eth.GetEtherTransferService().TransferEtherAndWaitForReceiptAsync(to, amount);
+                return receipt;
+            }
+
+            public async Task<decimal> GetBalance()
+            {
+                Console.WriteLine($"Address: {Address}");
+                BigInteger bal = await web3.Eth.GetBalance.SendRequestAsync(Address);
+                Console.WriteLine($"Balance (GWEI): {bal}");
+                decimal result = Web3.Convert.FromWei(bal);
+                Console.WriteLine($"Balance (BNB): {result}");
+                return result;
+            }
+        }
+        static string[] wordsList = File.ReadAllLines("Resources/words.txt");
+        static Wordlist words = Wordlist.LoadWordList(Language.English).Result;
+        public static string GenerateWordList()
+        {
+            random.Next();random.Next();random.Next();
+            string Words = "";
+            int count = 0;
+            wordsList = words.GetWords().ToArray<string>();
+            for (int j = 0; j<wordsList.Length;j++)
+            {
+                string word = wordsList[random.Next(0,wordsList.Length - 1)];
+                if (Words.Contains(word)) continue; // skip if the seed has the word already
+                Words += word;
+                count++;
+                if (count == 12) break; // Break out the loop once we have 12 words
+                Words += " "; // Add a space until the second to last word
+            }
+            return Words;
+        }
+
+        public static List<WalletAccount> accounts = new List<WalletAccount>();
+
+        static string path = "Resources/";
+        static string file = "BinanceWallets.json";
+        static string fullpath = path + file;
+
+        public static void Save()
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            File.WriteAllText(fullpath, JsonConvert.SerializeObject(accounts, Formatting.Indented));
+        }
+
+        public static void Load()
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            if (!File.Exists(fullpath))
+            {
+                File.WriteAllText(fullpath, JsonConvert.SerializeObject(accounts, Formatting.Indented));
+                string text = DateTime.Now + $" | Generated new file for binance wallets: {fullpath}";
+                Console.WriteLine(text);
+            }
+            else
+            {
+                string contents = File.ReadAllText(fullpath);
+                accounts = JsonConvert.DeserializeObject<List<WalletAccount>>(contents);
+            }
+
+        }
+        public static WalletAccount GetAccount(SocketGuildUser user)
+        {
+            if (user == null) return null;
+
+            foreach (WalletAccount account in accounts)
+                if (user.Id == account.DiscordID) return account;
+
+            return null;
+        }
+
+        public static WalletAccount CreateAccount(ulong discordID, string password = "ByscuitBros")
+        {
+            WalletAccount account = new WalletAccount(GenerateWordList(), password, discordID);
+            return account;
+        }
+
+        public static void AddAccountToList(WalletAccount account)
+        {
+            if(!accounts.Contains(account))
+            {
+                bool isDuplicate = false;
+                foreach(WalletAccount acc in accounts)
+                {
+                    if(acc.DiscordID == account.DiscordID)
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) { accounts.Add(account); Save(); }
+            }
+        }
+    }
+}
---
+++ ByscuitBotv2/Modules/BSCComs.cs
@@ -0,0 +1,237 @@
+﻿using ByscuitBotv2.Data;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByscuitBotv2.Modules
+{
+    public class BSCComs : ModuleBase<SocketCommandContext>
+    {
+        static string MAIN_NET = "https://bsc-dataseed1.binance.org:443";
+        static string TEST_NET = "https://data-seed-prebsc-1-s1.binance.org:8545";
+        static string CURRENT_NET = TEST_NET;
+        [Command("BNBRegister")]
+        [Alias("bscregister", "bnbenable", "bscenable")]
+        [Summary("Link your discord account to a Binance Smart Chain Wallet - Usage: {0}BNBRegister <password (optional)>")]
+        public async Task Register([Remainder]string password = "")
+        {
+            if (password == "") password = "ByscuitBros";
+            else await Context.Message.DeleteAsync();
+            SocketGuildUser user = Context.User as SocketGuildUser;
+            string username = (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username) + "#" + user.Discriminator;
+            BinanceWallet.WalletAccount account = BinanceWallet.GetAccount(user);
+            EmbedBuilder embed = new EmbedBuilder();
+            if (account != null)
+            {
+                await Context.Channel.SendMessageAsync($"> **{username}**_({user.Id})_ already has a BNB wallet linked!");
+                return;
+            }
+            string BNBIcon = "https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png";
+            string BNBUrl = "https://coinmarketcap.com/currencies/binance-coin/";
+            account = BinanceWallet.CreateAccount(user.Id, password);
+            BinanceWallet.AddAccountToList(account);
+            embed.WithAuthor("Binance Coin Wallet", BNBIcon, BNBUrl);
+            embed.WithThumbnailUrl(user.GetAvatarUrl());
+            embed.WithColor(36, 122, 191);
+            string message = $"`Successfully created and linked a new wallet to {username}`";
+            embed.Description = message;
+            embed.WithFields(new EmbedFieldBuilder[] { new EmbedFieldBuilder().WithIsInline(false).WithName("Address").WithValue(account.Address)});
+            embed.WithFooter(new EmbedFooterBuilder() { Text = "Block Number: " + await BinanceWallet.web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() });
+            embed.WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+
+        [Command("BNBWallet")]
+        [Alias("bscwallet", "bnb")]
+        [Summary("Show the amount of Binance Coins in your wallet - Usage: {0}BNBWallet")]
+        public async Task Wallet(SocketGuildUser user = null)
+        {
+            if (user == null) user = Context.User as SocketGuildUser;
+            string username = (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username) + "#" + user.Discriminator;
+            BinanceWallet.WalletAccount account = BinanceWallet.GetAccount(user);
+            if (account == null) 
+            {
+                await Context.Channel.SendMessageAsync($"> **{username}**_({user.Id})_ has no Binance Wallet linked!"+
+                    "\n> Use the **BNBRegister** command to link a wallet.");
+                return;
+            }
+            // Make it private message only later
+            //if (!Context.IsPrivate) await Context.Channel.SendMessageAsync($"> Use this command in a private message only!");
+            EmbedBuilder embed = new EmbedBuilder();
+            string BNBIcon = "https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png";
+            string BNBUrl = "https://coinmarketcap.com/currencies/binance-coin/";
+            embed.WithAuthor("Binance Coin Wallet", BNBIcon, BNBUrl);
+            embed.WithThumbnailUrl(user.GetAvatarUrl());
+            embed.WithColor(36, 122, 191);
+            embed.WithFields(
+                new EmbedFieldBuilder[]{ new EmbedFieldBuilder().WithIsInline(false).WithName(username).WithValue($"`{account.Address}`"),
+                new EmbedFieldBuilder().WithIsInline(true).WithName("Balance").WithValue(string.Format("{0:N8}", await account.GetBalance())),
+                new EmbedFieldBuilder().WithIsInline(true).WithName("Transaction Count").WithValue(string.Format("{0}", await BinanceWallet.web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(account.Address))),
+                // Display more... Creation date? Transaction Count?
+                });
+            embed.WithFooter(new EmbedFooterBuilder() { Text = "Block Number: " + await BinanceWallet.web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() });
+            embed.WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("BNBDeposit")]
+        [Alias("bscdeposit", "deposit", "depositbnb")]
+        [Summary("Show your deposit address for Binance Smart Chain wallet - Usage: {0}BNBDeposit")]
+        public async Task Deposit()
+        {
+            //decimal minimumDeposit = 0.01m;
+            var user = Context.User as SocketGuildUser;
+            string username = (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username) + "#" + user.Discriminator;
+            BinanceWallet.WalletAccount account = BinanceWallet.GetAccount(user);
+            EmbedBuilder embed = new EmbedBuilder();
+            if (account == null)
+            {
+                await Context.Channel.SendMessageAsync($"> **{username}**_({user.Id})_ has no Binance Wallet linked!" +
+                    "\n> Use the **BNBRegister** command to link a wallet.");
+                return;
+            }
+            string BNBIcon = "https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png";
+            string BNBUrl = "https://coinmarketcap.com/currencies/binance-coin/";
+            embed.WithAuthor("Binance Coin Wallet", BNBIcon, BNBUrl);
+            embed.WithThumbnailUrl(user.GetAvatarUrl());
+            embed.WithColor(36, 122, 191);
+            embed.WithFields(new EmbedFieldBuilder[]{ new EmbedFieldBuilder().WithIsInline(false).WithName(username).WithValue(account.Address)});
+            embed.Description = $"`Deposit only BEP-20 and Binance Chain Native Token.`";
+            embed.WithFooter(new EmbedFooterBuilder() { Text = "Block Number: " + await BinanceWallet.web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() });
+            embed.WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("BNBWithdraw")]
+        [Alias("bscwithdraw", "withdraw", "withdrawbnb")]
+        [Summary("Withdraw BNB from your Binance Smart Chain wallet - Usage: {0}BNBWithdraw <amount> <address>")]
+        public async Task Withdraw(decimal amount = -1, string address = "")
+        {
+            if (amount == -1 || address == "")
+            {
+                string msg = "> Withdraw command called incorrectly!" +
+                    "\n> Usage: **BNBWithdraw** *<amount>* *<address>*";
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+            decimal minimumWithdraw = 0.01m;
+            var user = Context.User as SocketGuildUser;
+            string username = (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username) + "#" + user.Discriminator;
+            BinanceWallet.WalletAccount account = BinanceWallet.GetAccount(user);
+            EmbedBuilder embed = new EmbedBuilder();
+            if (account == null)
+            {
+                await Context.Channel.SendMessageAsync($"> **{username}**_({user.Id})_ has no Binance Wallet linked!" +
+                    "\n> Use the **BNBRegister** command to link a wallet.");
+                return;
+            }
+
+            Nethereum.Util.AddressUtil addressUtil = new Nethereum.Util.AddressUtil();
+            if (!addressUtil.IsValidEthereumAddressHexFormat(address))
+            {
+                string msg = $"> The address {address} is not a valid address!"+
+                    $"> Double check you are entering it correctly!";
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+            string BNBIcon = "https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png";
+            string BNBUrl = "https://coinmarketcap.com/currencies/binance-coin/";
+            embed.WithAuthor("Binance Coin Withdraw", BNBIcon, BNBUrl);
+            embed.WithThumbnailUrl(user.GetAvatarUrl());
+            embed.WithColor(36, 122, 191);
+            bool minimumMet = amount >= minimumWithdraw;
+            if (!minimumMet)
+            {
+                string msg = $"> Minimum amount you can withdraw is {minimumWithdraw} BNB.";
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+            TransactionReceipt receipt = await account.SendBNB(address, amount);
+            if (receipt.Failed()) embed.Description = $"`Withdraw has failed.`";
+            else
+            {
+                embed.Description = $"`Withdraw has been sent.`";
+                embed.WithFields(new EmbedFieldBuilder[] {
+                    new EmbedFieldBuilder().WithIsInline(false).WithName(username).WithValue($"`{account.Address}`"),
+                    new EmbedFieldBuilder().WithIsInline(false).WithName("Hash").WithValue($"`{receipt.TransactionHash}`"),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Amount Sent").WithValue(amount),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Balance Left").WithValue(await account.GetBalance()),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Gas Used").WithValue(Web3.Convert.FromWei(receipt.GasUsed)),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Index").WithValue(receipt.TransactionIndex),
+                });
+            }
+            embed.WithFooter(new EmbedFooterBuilder() { Text = "Block Number: " + await BinanceWallet.web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() });
+            embed.WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+
+        [Command("BNBTip")]
+        [Alias("bsctip", "tip", "tipbnb")]
+        [Summary("Tip BNB from your Binance Smart Chain wallet to another user - Usage: {0}BNBTip <amount> <@user>")]
+        public async Task Tip(decimal amount = -1, SocketGuildUser receipient = null, [Remainder]string text = "")
+        {
+            decimal minimumTip = 0.0001m;
+            var user = Context.User as SocketGuildUser;
+            string username = (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username) + "#" + user.Discriminator;
+            if (amount == -1 || receipient == null)
+            {
+                string msg = "> Tip command called incorrectly!" +
+                    "\n> Usage: **BNBTip** *<amount>* *<@user>*";
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+            BinanceWallet.WalletAccount account = BinanceWallet.GetAccount(user);
+            var receipientAccount = BinanceWallet.GetAccount(receipient);
+            string rUsername = (!string.IsNullOrEmpty(receipient.Nickname) ? receipient.Nickname : receipient.Username) + "#" + receipient.Discriminator;
+            EmbedBuilder embed = new EmbedBuilder();
+            if (account == null)
+            {
+                await Context.Channel.SendMessageAsync($"> **{username}**_({user.Id})_ has no Binance Wallet linked!" +
+                    "\n> Use the **BNBRegister** command to link a wallet.");
+                return;
+            }
+            if (receipientAccount == null)
+            {
+                await Context.Channel.SendMessageAsync($"> **{rUsername}**_({receipient.Id})_ has no Binance Wallet linked!" +
+                    $"\n> {receipient.Mention} use the **BNBRegister** command to link a wallet.");
+                return;
+            }
+
+            string address = receipientAccount.Address;
+            string BNBIcon = "https://s2.coinmarketcap.com/static/img/coins/200x200/1839.png";
+            string BNBUrl = "https://coinmarketcap.com/currencies/binance-coin/";
+            embed.WithAuthor("Binance Coin Tip", BNBIcon, BNBUrl);
+            embed.WithThumbnailUrl(user.GetAvatarUrl());
+            embed.WithColor(36, 122, 191);
+            bool minimumMet = amount >= minimumTip;
+            if (!minimumMet)
+            {
+                string msg = $"> Minimum amount you can withdraw is {minimumTip} BNB.";
+                await Context.Channel.SendMessageAsync(msg);
+                return;
+            }
+            TransactionReceipt receipt = await account.SendBNB(address, amount);
+            if (receipt.Failed()) embed.Description = $"`Tip failed to send! Try again in a bit.`";
+            else
+            {
+                embed.Description = $"`Tip has been sent to {rUsername}`";
+                embed.WithFields(new EmbedFieldBuilder[] {
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Amount Sent").WithValue(amount),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Balance Left").WithValue(await account.GetBalance()),
+                    new EmbedFieldBuilder().WithIsInline(true).WithName("Gas Used").WithValue(Web3.Convert.FromWei(receipt.GasUsed))
+                });
+            }
+            embed.WithFooter(new EmbedFooterBuilder() { Text = "Block Number: " + await BinanceWallet.web3.Eth.Blocks.GetBlockNumber.SendRequestAsync() });
+            embed.WithCurrentTimestamp();
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+        }
+    }
+}
