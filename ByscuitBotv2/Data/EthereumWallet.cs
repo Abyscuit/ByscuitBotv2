99b2e781cf1ce490443252dde3283405d812d3b2
Marcus Escobedo <marcus14530@gmail.com>
2021-05-07 14:16:16 -0700

    Added EthereumWallet.cs

---
+++ ByscuitBotv2/Data/EthereumWallet.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByscuitBotv2.Data
+{
+    public class EthereumWallet
+    {
+
+    }
+}
