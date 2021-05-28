3ec9e7b5e0867949a4fa0a7fce64a7ca5948619e
Marcus Escobedo <marcus14530@gmail.com>
2021-05-28 03:22:21 -0700

    Added Add User function and had to change SocketGuildUser to RestGuildUser

--- ByscuitBotv2/Data/Account.cs
+++ ByscuitBotv2/Data/Account.cs
@@ -1,4 +1,5 @@
-﻿using Discord.WebSocket;
+﻿using Discord.Rest;
+using Discord.WebSocket;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -50,10 +51,10 @@ namespace ByscuitBotv2.Data
         {
             // Write the actual stuff
             guild.GetUsersAsync().ForEachAsync(m => {
-                foreach(SocketGuildUser user in m)
+                foreach(RestGuildUser user in m)
                 {
+                    if (user.IsBot) continue; // Dont create an account for the bots
                     Account account = new Account();
-                    if (user == null) continue;
                     account.discordID = user.Id;
                     account.discriminator = user.Discriminator;
                     account.username = user.Username;
@@ -66,15 +67,35 @@ namespace ByscuitBotv2.Data
                     Console.WriteLine("User: " + (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username));
                     Console.WriteLine("Total Days: " + totaldays);
                     double premiumTime = (user.PremiumSince.HasValue ? user.PremiumSince.Value.Subtract(DateTime.Now).TotalDays * -1 : 0);
-                    Console.WriteLine("Hierarchy: " + user.Hierarchy);
                     Console.WriteLine("Premium Days: " + premiumTime);
                     Console.WriteLine("Credits: " + account.credits + " | " + string.Format("{0:p}", (account.credits / totalcredits)) + "\n-------------------");
                 }
-            });
+            }).Wait();
             Console.WriteLine($"Total Credits Supply: {totalcredits:N0}");
             SaveFile();
         }
+        public static Account AddUser(SocketGuildUser user)
+        {
+            if (user.IsBot) return null; // Dont create an account for the bots
+            if (GetAccount(user) != null) return null; // Dont create duplicates
+            Account account = new Account();
+            account.discordID = user.Id;
+            account.discriminator = user.Discriminator;
+            account.username = user.Username;
+            account.credits = 0;
+            account.redeemed = null;
+            account.transactions = new List<Transaction>();
+            accounts.Add(account);
+            double totaldays = user.JoinedAt.Value.Subtract(DateTime.Now).TotalDays * -1;
 
+            Console.WriteLine("User: " + (!string.IsNullOrEmpty(user.Nickname) ? user.Nickname : user.Username));
+            Console.WriteLine("Total Days: " + totaldays);
+            double premiumTime = (user.PremiumSince.HasValue ? user.PremiumSince.Value.Subtract(DateTime.Now).TotalDays * -1 : 0);
+            Console.WriteLine("Premium Days: " + premiumTime);
+            Console.WriteLine("Credits: " + account.credits + " | " + string.Format("{0:p}", (account.credits / totalcredits)) + "\n-------------------");
+            SaveFile();
+            return account;
+        }
         public static void SaveFile()
         {
             File.WriteAllText(fullpath, JsonConvert.SerializeObject(accounts, Formatting.Indented));
