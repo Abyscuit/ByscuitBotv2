46a50959331ee4ff1713a7ce9b32c0c5b8ae2051
Marcus Escobedo <marcus14530@gmail.com>
2021-12-05 22:04:20 -0800

    Added SaveLog, printError, printLog, printDebug

--- ByscuitBotv2/Modules/Utility.cs
+++ ByscuitBotv2/Modules/Utility.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace ByscuitBotv2.Modules
     public class Utility
     {
         static Random rand = new Random();
+        public static List<string> log = new List<string>();
+        static uint DEBUG_LEVEL = 3; // 0 = OFF, 1 = ERROR, 2 = LOG, 3 = DEBUG || More settings in the future
+        //static string[] DEBUG_LVL_STR = { "OFF", "ERROR", "LOG", "DEBUG" };
 
         /// <summary>
         ///Print to console with the current timestamp
@@ -19,9 +23,23 @@ namespace ByscuitBotv2.Modules
         /// <param name="message">The message to be sent</param>
         public static void printConsole(string message)
         {
-            Console.WriteLine(DateTime.Now.ToLocalTime() + " | " + message);
+            string msg = DateTime.Now.ToLocalTime() + " | " + message;
+            Console.WriteLine(msg);
+            log.Add(msg);
+        }
+        public static void printERROR(string message)
+        {
+            if (DEBUG_LEVEL >= 1) printConsole($"ERROR | {message}");
         }
 
+        public static void printDEBUG(string message)
+        {
+            if (DEBUG_LEVEL >= 3) printConsole($"DEBUG | {message}");
+        }
+        public static void printLOG(string message)
+        {
+            if (DEBUG_LEVEL >= 2) printConsole($"LOG | {message}");
+        }
 
         public static async Task DirectMessage(SocketGuildUser user, string msg = "")
         {
@@ -32,7 +50,7 @@ namespace ByscuitBotv2.Modules
             // Print to console that we sent the message
             string username = user.Username + "#" + user.Discriminator;
             string text = $"{username} was sent a private message";
-            printConsole(text);
+            printLOG(text);
         }
         public static async Task DirectMessage(SocketGuildUser user,string msg = "", Embed embed = null)
         {
@@ -43,7 +61,7 @@ namespace ByscuitBotv2.Modules
             // Print to console that we sent the message
             string username = user.Username + "#" + user.Discriminator;
             string text = $"{username} was sent a private embedded message";
-            printConsole(text);
+            printLOG(text);
         }
 
         public static int RandomNum(int min, int max)
@@ -51,6 +69,22 @@ namespace ByscuitBotv2.Modules
             // Generate a few random calls first then return it
             rand.Next(min, max); rand.Next(min, max); rand.Next(min, max); rand.Next(min, max);
             return rand.Next(min, max);
+        }
+
+        public static void SaveLog()
+        {
+            string saveFolder = "Resources";
+            string saveFile = $"{DateTime.Now.ToString("dd")}";
+            string savePath = $"{saveFolder}/{saveFile}";
+            if (!Directory.Exists(saveFolder)) Directory.CreateDirectory(saveFolder);
+            string logString = log.Count > 0 ? log[0] : "";
+            for (int i = 1; i < log.Count; i++) { logString += log[i]; }
+            File.WriteAllText(savePath, logString);
+        }
+
+        public static void SetDebugLevel(uint dbgLvl)
+        {
+            DEBUG_LEVEL = dbgLvl;
         }
     }
 }
